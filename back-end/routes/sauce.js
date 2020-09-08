4c29406ad61d33126de3fc2007740f5a54bb89bd
dev-allan <62991985+dev-allan@users.noreply.github.com>
2020-09-08 10:41:49 +0200

    Update sauce.js
    Ajout de l'authentification avoir accès à toutes les sauces.

--- back-end/routes/sauce.js
+++ back-end/routes/sauce.js
@@ -4,7 +4,7 @@ const auth = require('../middleware/auth');
 const sauceCtrl = require('../controllers/sauce');
 const multer = require('../middleware/multer-config');
 
-router.get('/', sauceCtrl.getAllSauces);
+router.get('/', auth, sauceCtrl.getAllSauces);
 router.get('/:id',auth, sauceCtrl.getOneSauce);
 router.post('/',auth,multer, sauceCtrl.createSauce);
 router.put('/:id',auth,multer, sauceCtrl.modifySauce);
